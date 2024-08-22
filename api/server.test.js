0bbd9ce69d0f96da0637b739136859c7ff5691c2
brepp22 <brepp@hotmail.com>
2024-08-21 20:24:01 -0700

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require('../api/server')
+
+beforeAll(async () => {
+  await db.migrate.rollback() 
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('sanity check' , () => {
+  test('environment is testing' , () => {
+      expect(process.env.NODE_ENV).toBe('testing')
+  })
 })
